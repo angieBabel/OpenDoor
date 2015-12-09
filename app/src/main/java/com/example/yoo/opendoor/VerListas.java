8678397412035396b2a01301a711b43438fdb737
Angelica Barreda B <anjelyk.b@gmail.com>
2015-12-09 03:49:49 -0600

    REcuperación

--- app/src/main/java/com/example/yoo/opendoor/VerListas.java
+++ app/src/main/java/com/example/yoo/opendoor/VerListas.java
@@ -26,11 +26,9 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-<<<<<<< HEAD
 
 import java.util.ArrayList;
-=======
->>>>>>> origin/desarrollo
+
 
 public class VerListas extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Switch switchgrupo;
@@ -190,45 +188,5 @@ public class VerListas extends AppCompatActivity implements AdapterView.OnItemSe
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
-    }
-    public void showAlumnos(){
-        //spinner aula
-        Spinner spinnerAlumAc = (Spinner) findViewById(R.id.spinnerAlAc);
-        requestQueueAA = Volley.newRequestQueue(getApplicationContext());
-        //listaA(la);
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
-                showURL,new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response)  {
-                try {
-                    JSONArray alumnos = response.getJSONArray("alumnos");
-                    for (int i = 0; i < alumnos.length(); i++) {
-
-                        JSONObject alumno = alumnos.getJSONObject(i);
-                        String nocontrol = alumno.getString("nocontrol");
-                        String nombre = alumno.getString("nombre");
-                        listaAlumnos.add(nocontrol + "\n" + nombre);
-                        //listaA[i]=idaula;
-                    }
-                    dataAdapter.notifyDataSetChanged();
-
-                }catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                System.out.append(error.getMessage());
-
-            }
-        });
-
-        requestQueueAA.add(jsonObjectRequest);
-        dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,listaAlumnos);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerAlumAc.setAdapter(dataAdapter);
-        spinnerAlumAc.setOnItemSelectedListener(AlumnoActividad.this);
     }
 }
