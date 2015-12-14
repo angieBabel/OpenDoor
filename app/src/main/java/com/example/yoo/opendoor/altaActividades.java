8b5ad6506155f685d33d8f2e7bf5568ef4f73016
Angelica Barreda B <anjelyk.b@gmail.com>
2015-12-14 06:37:08 -0600

    Update

--- app/src/main/java/com/example/yoo/opendoor/altaActividades.java
+++ app/src/main/java/com/example/yoo/opendoor/altaActividades.java
@@ -32,8 +32,8 @@ public class altaActividades extends AppCompatActivity implements AdapterView.On
     String aulaAct=" ";
     Button registrarAct;
     RequestQueue requestQueueA;
-    //String insertURL= "http://192.168.43.64:8080/OpenDoor/insertActividad.php";
-    String insertURL= "http://192.168.1.66:8080/OpenDoor/insertActividad.php";
+    String insertURL= "http://192.168.43.64:8080/OpenDoor/insertActividad.php";
+    //String insertURL= "http://192.168.1.66:8080/OpenDoor/insertActividad.php";
     View v;
 
     //Traer datos
